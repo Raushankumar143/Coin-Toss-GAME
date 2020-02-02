938b7ca0361b31b8d4f72021a99fbc5c6d58c584
Raushankumar143 <55630365+Raushankumar143@users.noreply.github.com>
2020-02-02 21:02:52 +0530

    Add files via upload

---
+++ Coin_ToSS.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+    int c;
+cout<<"Welcome To Coin Toss GAME."<<endl;
+while(1){
+        cout<<"1.START..."<<endl;
+        cout<<"2.Quit..."<<endl;
+        cin>>c;
+        switch(c){
+            case 1:
+                if(rand()%2==0)
+                 {
+                    cout<<"HEAD"<<endl;
+                  }else{
+                     cout<<"TALL"<<endl;
+                }
+            case 2: exit(0);
+            default: cout<<"invalid input!"<<endl;
+        }
+}
+cout<<"Thank You";
+return 0;
+}
